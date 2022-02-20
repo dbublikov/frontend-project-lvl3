ae72dd08bc88b7ef774b388a4f6d5ba2290010bb
dbublikov <bublikow.dmitri@gmail.com>
2022-02-20 20:58:30 +0300

    added parser rss

--- src/init.js
+++ src/init.js
@@ -1,3 +1,33 @@
+import axios from 'axios';
+import onChange from 'on-change';
+import parserRSS from './parser';
+
+const render = (state) => {
+  console.log(state);
+};
+
 export default () => {
-  console.log('init successful');
+  const state = {
+    feeds: [],
+    posts: [],
+    error: null,
+  };
+
+  const watchedState = onChange(state, () => render(watchedState));
+
+  // https://ru.hexlet.io/lessons.rss
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#url_input');
+    axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${input.value}`)
+      .then((res) => { console.log(res); return res; })
+      .then((res) => parserRSS(res.data.contents))
+      .then(({ title, description, items }) => {
+        watchedState.feeds.push({ title, description });
+        watchedState.posts.push(...items);
+        console.log(state);
+      })
+      .catch((err) => console.log(err));
+  });
 };
